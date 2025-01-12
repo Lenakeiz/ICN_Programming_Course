59d9018e65abdd9fcf8ece2c2efc9dd1a59e36cb
Andrea <andrea.castegnaro@outlook.com>
2025-01-12 17:02:23 +0000

    added comments to the scripts

--- neuron.py
+++ neuron.py
@@ -2,12 +2,52 @@ import numpy as np
 import random
 
 class Neuron:
+    """
+    A class to represent a neuron in a neural network.
+    Attributes
+    ----------
+    index : int
+        The index of the neuron.
+    weights : np.array
+        The weights associated with the neuron's inputs.
+    is_refractory : bool
+        A flag indicating if the neuron is in a refractory period.
+    firing_threshold : float
+        The threshold above which the neuron will fire.
+    Methods
+    -------
+    __init__(self, index, weights, firing_threshold=0.5):
+        Initializes the neuron with an index, weights, and an optional firing threshold.
+    __str__(self):
+        Returns a string representation of the neuron.
+    weighted_sum(self, inputs):
+        Calculates the weighted sum of the inputs.
+    sigmoid(self, x):
+        Applies the sigmoid activation function to a given input.
+    fire(self, weighted_sum):
+        Determines if the neuron should fire based on the weighted sum and firing threshold.
+    process_inputs(self, inputs):
+        Processes the inputs through the neuron and returns the output.
+    reset_refractory(self):
+        Resets the refractory state of the neuron.
+    """
+
+    """
+    The __init__ method is a special function in Python. It’s called the initializer or constructor.
+    It automatically runs whenever you create a new object of the class.
+    It is used to set up the initial state of the object.
+    """
     def __init__(self, index, weights, firing_threshold=0.5):
         self.index = index
         self.weights = np.array(weights)
         self.is_refractory = False
         self.firing_threshold = firing_threshold
 
+    """
+    __str__ is another special function in Python.
+    When you call print(object) or str(object) on a neuron, this method determines what text gets displayed.
+    In this case, it will return a string that says something like: "Neuron 1 has 5 inputs".
+    """
     def __str__(self):
         return f"Neuron {self.index} has {len(self.weights)} inputs"
 
