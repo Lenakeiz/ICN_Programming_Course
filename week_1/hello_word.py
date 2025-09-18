0ffe0682746c0fb634c09d85e0f08511d5a361c3
Andrea <andrea.castegnaro@outlook.com>
2025-09-18 13:35:36 +0100

    week 1 updated

--- week_1/hello_word.py
+++ week_1/hello_word.py
@@ -20,4 +20,16 @@ pfc_label = "prefrontal"
 print(pfc_label.upper())     
 print(pfc_label.lower())
 
+# tab and newline characters
 print("ID\tAge\nS01\t23")
+
+# trim examples
+subject_name = "  S01  "
+print(subject_name.strip())
+print(subject_name.lstrip())
+print(subject_name.rstrip())
+
+# removing prefixes
+subject_name = "sub_1"
+subject_num = int(subject_name.removeprefix("sub_"))
+print(subject_num)
