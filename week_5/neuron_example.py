afd0a49d381da480c0d3b2a454c913da96e66281
Lenakeiz <andrea.castegnaro@outlook.com>
2025-09-30 16:04:11 +0100

    cleaned previous week 5

--- week_5/neuron_example.py
+++
@@ -1,9 +0,0 @@
-from neuron import Neuron
-import random
-
-weights = [random.uniform(-1, 1) for _ in range(5)] 
-neuron = Neuron(1,weights)
-inputs = [0.4, 0.3, 0.2, 0.1, 0.5]
-output = neuron.process_inputs(inputs)
-print("Neuron fired:", output)
-    
